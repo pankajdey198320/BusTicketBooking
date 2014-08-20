759bd23572a2a3a71430bb340816bb86e32ef7df
muhminch <abdulmuhaiminc@mindfiresolutions.com>
2014-08-20 23:23:29 +0530

    Changes:- Added boostrap and Jquery reference in a bundle

--- BusTicketBooking/App_Start/BundleConfig.cs
+++ BusTicketBooking/App_Start/BundleConfig.cs
@@ -18,6 +18,13 @@ namespace BusTicketBooking
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrapjs").Include(
+                        "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrapcss").Include(
+                        "~/Content/bootstrap.min.css",
+                        "~/Content/bootstrap-responsive.min.css"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
