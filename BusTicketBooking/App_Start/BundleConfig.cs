20b4d9552cabda5d4115c2bf0e49372d5efac3f5
muhminch <abdulmuhaiminc@mindfiresolutions.com>
2014-08-25 23:30:01 +0530

    added bootstrap bundle and jquery ui bundle

--- BusTicketBooking/App_Start/BundleConfig.cs
+++ BusTicketBooking/App_Start/BundleConfig.cs
@@ -18,6 +18,11 @@ namespace BusTicketBooking
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrapjs").Include(
                         "~/Scripts/bootstrap.min.js"));
 
@@ -25,16 +30,11 @@ namespace BusTicketBooking
                         "~/Content/bootstrap.min.css",
                         "~/Content/bootstrap-responsive.min.css"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
+          .Include("~/Scripts/bootstrap.js")
+          );
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
-            .Include("~/Scripts/bootstrap.js")
-            );
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
