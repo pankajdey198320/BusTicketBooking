99784d6c7cd7090ca0e1ac90249aa06c23193214
muhminch <abdulmuhaiminc@mindfiresolutions.com>
2014-08-28 00:40:05 +0530

    Added JSON method for Showing Cities in the textbox of Fromcity and Tocity

--- BusTicketBooking/Controllers/HomeController.cs
+++ BusTicketBooking/Controllers/HomeController.cs
@@ -3,17 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BusBookingTicket.Model;
 
 namespace BusTicketBooking.Controllers
 {
     public class HomeController : Controller
     {
+        BusBookingTicketEntities context = new BusBookingTicketEntities();
+
         public ActionResult Index()
         {
-            
             return View();
         }
 
+        
+        public JsonResult ShowCityNames(string strTerm)
+        {
+            var result = (from c in context.CityDetails
+                          where c.CityName.ToLower().Contains(strTerm.ToLower())
+                          select new { c.CityName }).Distinct();
+                    
+
+            return Json(result , JsonRequestBehavior.AllowGet);
+        
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your app description page.";
